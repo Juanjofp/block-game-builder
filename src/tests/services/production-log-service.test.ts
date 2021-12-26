8cc6ae3a005ed2eeb407626957ed0f0172bffc84
juanjo.franco <juanjo.franco@digio.es>
2021-12-26 12:27:54 +0100

    Fix production-log-service tests

--- src/tests/services/production-log-service.test.ts
+++ src/tests/services/production-log-service.test.ts
@@ -1,15 +1,36 @@
 import { loadProductionLog } from '../../services/log/framework/production-log-service';
 
 describe('loadProductionLog should', () => {
+    let spyLog: jest.SpyInstance;
+    beforeEach(() => {
+        spyLog = jest.spyOn(console, 'log');
+        spyLog.mockImplementation(() => {});
+    });
+    afterEach(() => {
+        spyLog.mockRestore();
+    });
+
     it('load undefined when production', () => {
         // @ts-ignore
         process.env.NODE_ENV = 'production';
-        expect(loadProductionLog()).toBeUndefined();
+        const log = loadProductionLog();
+
+        expect(log).toBeUndefined();
     });
 
-    it('load consoleLogProvider when test', () => {
+    it('load consoleLogService when test', () => {
         // @ts-ignore
         process.env.NODE_ENV = 'test';
-        expect(loadProductionLog()).not.toBeUndefined();
+        const log = loadProductionLog();
+
+        log?.info('print test message');
+
+        expect(log).not.toBeUndefined();
+        expect(spyLog).toHaveBeenCalledTimes(1);
+        expect(spyLog).toHaveBeenNthCalledWith(
+            1,
+            'INFO',
+            '"print test message"'
+        );
     });
 });
